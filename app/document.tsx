55fcce8465febdf58156c29d94a1927062d75913
angelesmarin <angeles.marinbatana@bsu.edu>
2024-10-22 05:43:38 -0400

    add document.tsx

--- app/document.tsx
+++ app/document.tsx
@@ -1,11 +1,29 @@
-// import { View, Button, Text, StyleSheet, Alert } from 'react-native';
-// import React, { useState } from 'react';
-// //import lib for documents 
+import { View, Button, Text, StyleSheet, Alert } from 'react-native';
+import React, { useState } from 'react';
+//import lib for documents 
 
-// /* 
-//   'DOCUMENT'PAGE  
-// */
+/* 
+  'DOCUMENT'PAGE  
+*/
 
-// export default function DocumentManagement() {
-//     const[document]
-// }
+export default function DocumentManagement() {
+    const[document, setDocument] = useState(null) //no document uploaded yet 
+
+    const pickDoc = () => {
+    }
+    return (
+        <View style={styles.container}>
+          <Button 
+            title="Select a Document (PDF)" 
+          />
+        </View>
+      );
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#e8f4f8',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
