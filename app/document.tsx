0d93edf6c1bf1e681103ebc57f6a1e37a16d4da5
Ehoffman723 <ethan.hoffman@bsu.edu>
2024-10-24 16:03:40 -0400

    Implement button that allows user to select document they want to upload

--- app/document.tsx
+++ app/document.tsx
@@ -1,29 +1,52 @@
-import { View, Button, Text, StyleSheet, Alert } from 'react-native';
+import { View, Button, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
 import React, { useState } from 'react';
+import * as DocumentPicker from "expo-document-picker"
 //import lib for documents 
 
 /* 
   'DOCUMENT'PAGE  
 */
 
-export default function DocumentManagement() {
-    const[document, setDocument] = useState(null) //no document uploaded yet 
 
-    const pickDoc = () => {
-    }
-    return (
-        <View style={styles.container}>
-          <Button 
-            title="Select a Document (PDF)" 
+
+const UploadFile = () => {
+  const pickDocument = async () => {
+    let result = await DocumentPicker.getDocumentAsync({});
+    console.log(result.url);
+    console.log(result);
+  };
+
+  return (
+    <View style={styles.background}>
+      <Text style={styles.file}>Upload CSV File</Text>
+      <View style={styles.button}>
+        <TouchableOpacity>
+          <Button
+            title="upload your file"
+            color="black"
+            onPress={pickDocument}
           />
-        </View>
-      );
+        </TouchableOpacity>
+      </View>
+    </View>
+  ); 
 }
+
+
 const styles = StyleSheet.create({
-    container: {
+    background: {
       flex: 1,
       backgroundColor: '#e8f4f8',
       alignItems: 'center',
       justifyContent: 'center',
     },
+    file: {
+      color: "black",
+      marginHorizontal: 145,
+    },
+    button: {
+      marginHorizontal: 60,
+    },
   });
+
+export default UploadFile;
