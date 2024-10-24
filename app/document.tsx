13e67d19d61f4b2ef6740846013119f410dff85d
Ehoffman723 <ethan.hoffman@bsu.edu>
2024-10-24 18:34:57 -0400

    Added image capability for the documents

--- app/document.tsx
+++ app/document.tsx
@@ -1,4 +1,4 @@
-import { View, Button, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import { View, Button, Text, StyleSheet, TouchableOpacity, Image, Alert } from 'react-native';
 import React, { useState } from 'react';
 import * as DocumentPicker from "expo-document-picker"
 //import lib for documents 
@@ -10,24 +10,43 @@ import * as DocumentPicker from "expo-document-picker"
 
 
 const UploadFile = () => {
+  const [fileUri,  setFileUri] = useState(null);
+  const [fileType, setFileType] = useState(null);
+
   const pickDocument = async () => {
-    let result = await DocumentPicker.getDocumentAsync({});
-    console.log(result.url);
-    console.log(result);
+    try{
+      let result = await DocumentPicker.getDocumentAsync({});
+      if (result.type === 'success') {
+        setFileUri(result.uri);
+        setFileType(result.name.split('.').pop());
+        console.log('File URI: ', result.uri);
+        console.log('File Name: ', result.name);
+      }
+    } catch (error) {
+      console.error('Error picking document: ', error);
+    }
   };
 
   return (
     <View style={styles.background}>
-      <Text style={styles.file}>Upload CSV File</Text>
+      <Text style={styles.file}>Upload File</Text>
       <View style={styles.button}>
         <TouchableOpacity>
           <Button
             title="upload your file"
-            color="black"
+            color="#1e90ff"
             onPress={pickDocument}
           />
         </TouchableOpacity>
       </View>
+
+      {/* Display the image if the file is an image */}
+      {fileUri && (fileType === 'jpg' || fileType === 'jpeg' || fileType === 'png') && (
+        <Image
+          source={{ uri: fileUri }}
+          style={{ width: 200, height: 200, marginTop: 20 }}
+        />
+      )}
     </View>
   ); 
 }
