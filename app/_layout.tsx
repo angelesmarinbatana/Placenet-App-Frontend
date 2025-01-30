ea0abe4dfe4d647c10ea606c49c151d8c5fecc1b
Ehoffman723 <ethan.hoffman@bsu.edu>
2025-01-30 15:22:28 -0500

    Remove ability to swipe to change pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,16 +4,16 @@ export default function RootLayout() {
   return (
     //PAGES IN APP: 
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Welcome' }} />
+      <Stack.Screen name="index" options={{ title: 'Welcome', headerShown: false, gestureEnabled: false }} />
       <Stack.Screen name="main" options={{ title: 'Main Menu' }} />
       <Stack.Screen name="property" options={{ title: 'Property Management' }} />
       <Stack.Screen name="project" options={{ title: 'Project Management' }} />
       <Stack.Screen name="document" options={{ title: 'Document Management' }} />
       <Stack.Screen name="property_summary" options={{ title: 'Property Summary' }} />
-      <Stack.Screen name="sign_in" options={{ title: 'Sign In', headerShown: false }} />
-      <Stack.Screen name="sign_up" options={{ title: 'Sign Up', headerShown: false }} />
+      <Stack.Screen name="sign_in" options={{ title: 'Sign In', headerShown: false, gestureEnabled: false }} />
+      <Stack.Screen name="sign_up" options={{ title: 'Sign Up', headerShown: false, gestureEnabled: false }} />
       <Stack.Screen name="listing_summaries" options={{ title: 'Social Project Summaries' }} />
-      <Stack.Screen name="navigation_bar" options={{ title: 'Back', headerShown: false }} />
+      <Stack.Screen name="navigation_bar" options={{ title: 'Back', headerShown: false, gestureEnabled: false }} />
       <Stack.Screen name="settings_page" options={{ title: 'Settings' }} />
     </Stack>
   );
